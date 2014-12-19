dc95d255ae125d2e425f0f6e5ec3dbc12c0f5104
Phi Nguyen <phi.nguyen@infonam.com>
2014-12-19 23:30:09 +0700

    - Added prefix file

---
+++ Hungary Radio Live/Hungary Radio Live/Common/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Hungary Radio Live
+//
+//  Created by Phi Nguyen on 12/19/14.
+//  Copyright (c) 2014 Thien Nguyen. All rights reserved.
+//
+
+#ifndef Hungary_Radio_Live_Constants_h
+#define Hungary_Radio_Live_Constants_h
+#define TEST_TEST @"TEST......."
+
+#endif
