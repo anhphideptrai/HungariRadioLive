86069dee5ab34bb129d1abd799c629db61e7595c
Phi Nguyen <phi.nguyen@infonam.com>
2014-12-20 00:16:35 +0700

    - Added: Images resource and Viewcontrollers

---
+++ Hungary Radio Live/Hungary Radio Live/ViewControllers/ChannelsViewController/ChannelsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ChannelsViewController.m
+//  Hungary Radio Live
+//
+//  Created by Phi Nguyen on 12/20/14.
+//  Copyright (c) 2014 Thien Nguyen. All rights reserved.
+//
+
+#import "ChannelsViewController.h"
+
+@interface ChannelsViewController ()
+
+@end
+
+@implementation ChannelsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
