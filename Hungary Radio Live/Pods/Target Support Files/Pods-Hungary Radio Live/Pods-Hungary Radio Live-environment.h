944d33a475bb36e536cd9155764e3a88ae054972
Phi Nguyen <phi.nguyen@infonam.com>
2014-12-20 22:11:27 +0700

    - Added libs

--- Hungary Radio Live/Pods/Target Support Files/Pods-Hungary Radio Live/Pods-Hungary Radio Live-environment.h
+++ Hungary Radio Live/Pods/Target Support Files/Pods-Hungary Radio Live/Pods-Hungary Radio Live-environment.h
@@ -6,6 +6,24 @@
 // project.
 
 
+// MarqueeLabel
+#define COCOAPODS_POD_AVAILABLE_MarqueeLabel
+#define COCOAPODS_VERSION_MAJOR_MarqueeLabel 2
+#define COCOAPODS_VERSION_MINOR_MarqueeLabel 1
+#define COCOAPODS_VERSION_PATCH_MarqueeLabel 0
+
+// SCSiriWaveformView
+#define COCOAPODS_POD_AVAILABLE_SCSiriWaveformView
+#define COCOAPODS_VERSION_MAJOR_SCSiriWaveformView 1
+#define COCOAPODS_VERSION_MINOR_SCSiriWaveformView 0
+#define COCOAPODS_VERSION_PATCH_SCSiriWaveformView 1
+
+// StreamingKit
+#define COCOAPODS_POD_AVAILABLE_StreamingKit
+#define COCOAPODS_VERSION_MAJOR_StreamingKit 0
+#define COCOAPODS_VERSION_MINOR_StreamingKit 1
+#define COCOAPODS_VERSION_PATCH_StreamingKit 25
+
 // iOS-Slide-Menu
 #define COCOAPODS_POD_AVAILABLE_iOS_Slide_Menu
 #define COCOAPODS_VERSION_MAJOR_iOS_Slide_Menu 1
